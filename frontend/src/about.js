563273bda72efac4b878098ad992186e76537391
Willian Oliveira <wijloc@gmail.com>
2020-11-19 15:52:31 -0300

    feat: add content to about page

--- frontend/src/about.js
+++ frontend/src/about.js
@@ -5,7 +5,8 @@ function About() {
   return (
     <>
       <h1>Gerenciamento de Tempo</h1>
-      <p>Feito por Willian Oliveira</p>
+      <p>Feito por Willian Oliveira como desafio na Expansion Week da Rocketseat!</p>
+      <p>Entre em contato pelas redes sociais</p>
     </>
   )
 }
