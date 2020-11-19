0f82029452fbeedd55322ef942327c22c0b53c4c
Willian Oliveira <wijloc@gmail.com>
2020-11-19 17:11:16 -0300

    feat: update content of about

--- src/about.js
+++ src/about.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './styles/about.css';
 
 function About() {
 
   return (
     <>
-      <h1>Gerenciamento de Tempo</h1>
-      <p>Feito por Willian Oliveira como desafio na Expansion Week da Rocketseat!</p>
-      <p>Entre em contato pelas redes sociais</p>
+      <div class="about-content">
+        <h1>Gerenciamento de Tempo</h1>
+        <p>Feito por Willian Oliveira como desafio na Expansion Week da Rocketseat!</p>
+        <p>Entre em contato pelas redes sociais</p>
+      </div>
     </>
   )
 }
