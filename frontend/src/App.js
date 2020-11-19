f9fca3b8838a813bd452e08b5538bade5f2b5788
Willian Oliveira <wijloc@gmail.com>
2020-11-18 23:34:23 -0300

    buid: init project
    install babel, webpack and the first component: header

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {  
+
+  return (
+    <>      
+      <Header title="Gerenciamento de Tempo" />
+    </>
+  )
+}
+
+export default App;
