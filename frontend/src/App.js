792b91b3f7086dfb2d97edbcbfbba94faab1f09f
Willian Oliveira <wijloc@gmail.com>
2020-11-19 10:58:58 -0300

    config: editor config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 
-function App() {  
+function App() {
 
   return (
-    <>      
+    <>
+      <div id ="">
       <Header title="Gerenciamento de Tempo" />
+      </div>
     </>
   )
 }
